79462d2f0f01b5e638d10242c6aa39c3981e7651
Doug Enas <douglassenas@gmail.com>
2018-11-05 17:31:13 -0500

    step 8 completed

---
+++ queries/02_create_posts_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS posts (
+        id serial PRIMARY KEY,
+        username varchar(15),
+        bio varchar(500)
+        user_id integer
+
+    );
---
+++ queries/04_insert_posts.sql
@@ -0,0 +1,5 @@
+INSERT INTO posts (title, body, user_id)
+VALUES ('Admin announcement', 'This API is awesome!', user_id)
+
+INSERT INTO posts (title, body, user_id)
+VALUES ('Just a test', 'This is just a test. I repeat, this is just a test.', user_id)
